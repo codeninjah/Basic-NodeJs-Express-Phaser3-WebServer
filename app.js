d38e4b76ef649420ca47fbfcc82762aaac5cb6b2
codeninjah <codeninjahblog@gmail.com>
2022-04-08 13:52:23 +0200

    Here's a basic web server for running your Phase3 projects

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const es6Renderer = require('express-es6-template-engine')
+
+const app = express()
+
+app.engine('html', es6Renderer);
+app.set('views', 'views');
+app.set('view engine', 'html');
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('part10.html')
+})
+
+app.listen(8000, () => {
+    console.log("Up and running")
+})
