c40f5e642b37f6e71afe4d32bb8c50eb4b931cbd
CodeNinjah <codeninjahblog@gmail.com>
2022-06-22 20:45:44 +0200

    Playing with camera to follow player

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'html');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('part1000.html')
+    res.render('2.html')
 })
 
 app.listen(8000, () => {
